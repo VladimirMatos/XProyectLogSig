9ea61ff6e4d7af08a03b131981d4ce0dc0851353
VladimirMatos <60585676+VladimirMatos@users.noreply.github.com>
2020-02-13 15:21:05 -0400

    XamarinProyc

---
+++ XamarinSigandLogPage/XamarinSigandLogPage/LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinSigandLogPage
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage(string Email)
+        {
+            InitializeComponent();
+            EntryUsername.Text = Email;
+        }
+
+        void IcShowandHidePass(System.Object sender, System.EventArgs e)
+        {
+            icPassShow.Source = !EntryPass.IsPassword ? "ic_show" : "ic_hide";
+            EntryPass.IsPassword = !EntryPass.IsPassword;
+
+        }
+        async void GoToSignUpTapped(object sender, EventArgs e)
+        {
+           await Navigation.PushModalAsync(new SignUpPage(EntryUsername.Text,EntryPass.Text));
+        }
+        async void ButtonLogin(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(EntryUsername.Text) || string.IsNullOrEmpty(EntryPass.Text))
+            {
+                await DisplayAlert("Alert", "You have been alerted", "OK");
+            }
+            else
+            {
+                await Navigation.PushModalAsync(new DiscoveryPage());
+            }
+           
+        }
+    }
+}
